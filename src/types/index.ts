59f06ff4466799ff704fc76e5156dd524c38a3d7
Mike Doubintchik <mike.doubintchik@gmail.com>
2025-04-25 12:06:50 -0400

    refactor: reorganize types into domain-specific files - Split types into image.ts, message.ts, platform.ts - Update index.ts to properly re-export types - Fix type re-export issues with export type - Improve type organization and maintainability

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+import {SelectedImage} from './image';
+import {
+  GenerateReplyRequest,
+  GenerateReplyResponse,
+  MessageLimit,
+  MessageStyle,
+} from './message';
+import {Platform} from './platform';
+import {SubscriptionTier, User} from './subscription';
+
+export {MessageStyle, Platform, SubscriptionTier};
+
+export type {
+  GenerateReplyRequest,
+  GenerateReplyResponse,
+  MessageLimit,
+  SelectedImage,
+  User,
+};
