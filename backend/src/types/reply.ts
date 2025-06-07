28a04ed80d28ec3b1a67844736865167a28dd572
Mike Doubintchik <mike.doubintchik@gmail.com>
2025-06-07 14:38:21 -0400

    Updates

--- backend/src/types/reply.ts
+++
@@ -1,9 +0,0 @@
-export interface GenerateReplyRequest {
-  message: string;
-  context?: string;
-}
-
-export interface GenerateReplyResponse {
-  reply: string;
-  error?: string;
-}
--- src/test/mocks/fileMock.js
+++
@@ -1,2 +0,0 @@
-// Mock for static files (images, fonts, etc.)
-module.exports = 'test-file-stub';
